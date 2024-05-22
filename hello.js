6e679b275b9d504f931dfa9beb49870483cfee7e
ceasinugo <ceasinugo@wustl.edu>
2024-05-21 20:24:59 -0500

    First Upload

--- hello.js
+++ hello.js
@@ -4,19 +4,40 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+x = "Hello ";
+y = 'World';
+z = "8: \"Hello World\"";
+
+console.log("Hello World");
+
+console.log("1: Goodbye World");
+console.log("2: Parentheses needed to initialize string");
+console.log("3: Double-quoted strings can contain single quotes inside them");
+console.log("4: Hello World")
+console.log(3 + 2);
+console.log("6:",x,y);
+console.log("7: " + x + y);
+console.log(z);
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
+
    2. Figure out what the parentheses do. Will the code work without them?
+
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+
    4. Remove the semi-colon, ;.
+
    5. Print a number. (Bonus: Print two numbers added together).
+
    6. Print multiple messages one after the other.
+
    7. Print two messages on the same line.
+
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+
    9. Other. You choose!
 */
